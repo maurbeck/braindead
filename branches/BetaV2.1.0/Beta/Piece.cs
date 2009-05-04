9a0794d45651692c90af9efb68c671bfa73f38a1
aduggan88 <aduggan88@ff848ea0-f35b-11dd-88c7-29a3b14d5316>
2009-05-04 15:10:55 +0000

    Added the new version number that will hold the new states of the game. There are NO changes in this revision besides adding a version number.

---
+++ branches/BetaV2.1.0/Beta/Piece.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Beta
+{
+    class Piece : Animation
+    {
+        // Derives all variables from Animation
+        // Animation derives all variables from Image
+        // Piece specific variables
+        Texture2D redTex;
+        Texture2D greenTex;
+        Texture2D redToGreen;
+        Texture2D greenToRed;
+        Texture2D transToPlr1;
+        Texture2D transToPlr2;
+        int state;
+
+
+        private enum State
+        {
+            Blank = 0,
+            Red = 1,
+            Green = 2,
+            RedToGreen = 3,
+            GreenToRed = 4,
+            TransToPlr1 = 5,
+            TransToPlr2 = 6
+        }
+
+        // Constructor
+        public Piece()
+        {
+            // Do nothing
+        }
+
+        // Initialize all required variables
+        public void Initialize(Vector2 pos, Rectangle src, Vector2 origin, Vector2 scale, Single depth)
+        {
+            // Initialize user defined variables
+            this.position = pos;
+            this.source = src;
+            this.origin = origin;
+            this.depth = depth;
+            this.scale = scale;
+
+            // Initialize constant variables
+            //this.state = (int)State.Blank;
+            this.fps = 25;
+            if (Board.boardFull == true)
+                CalculateDelay2();
+            else
+                CalculateDelay();
+            this.loop = false;
+            this.started = false;
+            this.curFrame = 1;
+            this.numFrame = 10;
+            this.color = Color.White;
+            this.rotation = 0;
+            this.effects = SpriteEffects.None;
+        }
+
+        // Load all required graphics content
+        public void LoadContent(SpriteBatch spriteBatch, Texture2D red, Texture2D green, Texture2D redGreen, Texture2D greenRed, Texture2D tPlr1, Texture2D tPlr2)
+        {
+            this.spriteBatch = spriteBatch;
+            this.redTex = red;
+            this.greenTex = green;
+            this.redToGreen = redGreen;
+            this.greenToRed = greenRed;
+            this.transToPlr1 = tPlr1;
+            this.transToPlr2 = tPlr2;
+        }
+
+        // Update the animations
+        new public void Update(GameTime gameTime)
+        {
+            if (started)
+            {
+                time += Math.Max(1, gameTime.ElapsedGameTime.Milliseconds);
+                if (time >= delay)
+                {
+                    curFrame++;
+                    if (curFrame >= numFrame)
+                    {
+                        // Animation.Reset()
+                        Reset();
+                        // Set the state to the correct static state
+                        if (state == (int)State.RedToGreen)
+                        {
+                            state = 2;
+                        }
+                        if (state == (int)State.GreenToRed)
+                        {
+                            state = 1;
+                        }
+                        if (state == (int)State.TransToPlr1)
+                        {
+                            state = 1;
+                        }
+                        if (state == (int)State.TransToPlr2)
+                        {
+                            state = 2;
+                        }
+                    }
+                    CalculateSource();
+                    time = 0;
+                }
+            }
+        }
+
+        // Called by the animation handler to start the animation
+        // This is just an alias for Start()
+        public void Animate()
+        {
+            // Simply call start()
+            Start();
+        }
+
+        // Will set this to animate from the current state to toState when
+        // the animation handler starts the animation
+        public void Mutate(int toState)
+        {
+            // If toState is red
+            if (toState == (int)State.Red)
+            {
+                state = (int)State.GreenToRed;
+            }
+            // If toState is green
+            if (toState == (int)State.Green)
+            {
+                state = (int)State.RedToGreen;
+            }
+        }
+
+        // Set the state directly with no animations
+        public void SetState(int toState)
+        {
+            Reset();
+            CalculateSource();
+            this.state = toState;
+        }
+
+        // Return the value of the current state
+        public int Value()
+        {
+            return state;
+        }
+        // Draw to the screen
+        new public void Draw()
+        {
+            switch(state)
+            {
+                case 0:
+                    // Dont draw anything (blank)
+                    break;
+                case 1:
+                    spriteBatch.Begin();
+                    spriteBatch.Draw(this.redTex, this.position, this.source, this.color, this.rotation, this.origin, this.scale, this.effects, this.depth);
+                    spriteBatch.End();
+                    break;
+                case 2:
+                    spriteBatch.Begin();
+                    spriteBatch.Draw(this.greenTex, this.position, this.source, this.color, this.rotation, this.origin, this.scale, this.effects, this.depth);
+                    spriteBatch.End();
+                    break;
+                case 3:
+                    spriteBatch.Begin();
+                    spriteBatch.Draw(this.redToGreen, this.position, this.source, this.color, this.rotation, this.origin, this.scale, this.effects, this.depth);
+                    spriteBatch.End();
+                    break;
+                case 4:
+                    spriteBatch.Begin();
+                    spriteBatch.Draw(this.greenToRed, this.position, this.source, this.color, this.rotation, this.origin, this.scale, this.effects, this.depth);
+                    spriteBatch.End();
+                    break;
+                case 5:
+                    spriteBatch.Begin();
+                    spriteBatch.Draw(this.transToPlr1, this.position, this.source, this.color, this.rotation, this.origin, this.scale, this.effects, this.depth);
+                    spriteBatch.End();
+                    break;
+                case 6:
+                    spriteBatch.Begin();
+                    spriteBatch.Draw(this.transToPlr2, this.position, this.source, this.color, this.rotation, this.origin, this.scale, this.effects, this.depth);
+                    spriteBatch.End();
+                    break;
+            }
+        }
+    }
+}
---
+++ branches/BetaV2.1.0/Beta/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jojamian")]
+[assembly: AssemblyProduct("Jojamian")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Project jojamian")]
+
+[assembly: AssemblyCopyright("Copyright © Project Jojamian 2009")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bd4fc82d-b7c6-4d66-ad99-c42bfd403a5c")]
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
